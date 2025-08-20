83541fc8d525c132f0316e92471fe884f02ff802
stevanovpetar <petarstevanov.ja@gmail.com>
2025-08-20 13:17:20 +0200

    Refaktorisanje koda – izdvajanje operacija u posebnu klasu

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -10,35 +10,16 @@
 public class Kalkulator {
     public static void main(String[] args) {
         System.out.println("Dobrodošli u kalkulator!");
-        int zbir = saberi(5, 3);
-        int razlika = oduzmi(5, 3);
-        int proizvod = pomnozi(5, 3);
-        double kolicnik = podeli(10, 2);
+
+        int zbir = Operacije.saberi(5, 3);
+        int razlika = Operacije.oduzmi(5, 3);
+        int proizvod = Operacije.pomnozi(5, 3);
+        double kolicnik = Operacije.podeli(10, 2);
 
         System.out.println("Sabiranje: 5 + 3 = " + zbir);
         System.out.println("Oduzimanje: 5 - 3 = " + razlika);
         System.out.println("Množenje: 5 * 3 = " + proizvod);
         System.out.println("Deljenje: 10 / 2 = " + kolicnik);
     }
-
-    public static int saberi(int a, int b) {
-        return a + b;
-    }
-
-    public static int oduzmi(int a, int b) {
-        return a - b;
-    }
-
-    public static int pomnozi(int a, int b) {
-        return a * b;
-    }
-
-    public static double podeli(int a, int b) {
-        if (b == 0) {
-            System.out.println("Greška: deljenje sa nulom nije moguce!");
-            return 0;
-        }
-        return (double) a / b;
-    }
 }
-
+  
