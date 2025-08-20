b41194c070046e840cf22ca0d96d887135492117
stevanovpetar <petarstevanov.ja@gmail.com>
2025-08-20 12:45:50 +0200

    Dodavanje funkcije za sabiranje

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -8,9 +8,13 @@
  * @author Petar
  */
 public class Kalkulator {
-
     public static void main(String[] args) {
         System.out.println("Dobrodošli u kalkulator!");
+        int rezultat = saberi(5, 3);
+        System.out.println("Sabiranje: 5 + 3 = " + rezultat);
     }
-    
+
+    public static int saberi(int a, int b) {
+        return a + b;
+    }
 }
