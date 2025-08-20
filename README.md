7d1b719d8089fe0162c9c06e28676876bb2ce935
stevanovpetar <petarstevanov.ja@gmail.com>
2025-08-20 12:38:40 +0200

    Početna verzija projekta – main funkcija.

---
+++ README.md
@@ -0,0 +1,4 @@
+# SZUP_projekat_kalkulator
+
+Ovaj projekat je jednostavan kalkulator implementiran u Javi, napravljen kao deo zadatka iz predmeta *Sistemi za upravljanje dokumentima*.
+Projekat demonstrira osnovne matematičke operacije (sabiranje, oduzimanje, množenje i deljenje) i integraciju NetBeans okruženja sa GitHub repozitorijumom kroz rad sa granama, commit-ovima i verzijama.
---
+++ src/Kalkulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Petar
+ */
+public class Kalkulator {
+
+    public static void main(String[] args) {
+        System.out.println("Dobrodošli u kalkulator!");
+    }
+    
+}
